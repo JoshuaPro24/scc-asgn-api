480c552e61ed8a9d0bb827000039ae260fb6ff1e
Joshua <JoshuaPro24@users.noreply.github.com>
2022-05-02 14:03:06 -0500

    Second_Commit

--- src/app.js
+++ src/app.js
@@ -16,14 +16,13 @@ app.use(assignmentRoute);
 
 app.use(express.static('public'));
 
-
 app.use((req, res, next) => {
     res.status(404).send(`${req.originalUrl} not found!`);
 });
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.sendFile(path.join(__dirname, '../public/500.html'));
+    res.status(500).send(`There was an issue!`);
 });
 
 
